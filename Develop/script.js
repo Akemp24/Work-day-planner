93dcc0005d88ccb6d0d028305b2575b85f9b98e0
Asiak <asiakemp24@gmail.com>
2023-10-25 20:10:39 -0400

    added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,24 @@
-var currentTime = dayjs().hour();
- // write function for the day
- $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// app works correctly by running JS code after HTML loads
   $(function () {
+    // getting current time using dayjs library
+    var currentTime = dayjs().hour();
+    // write function for the day
+    // sets content of the element to the current date in the format of day of week, month, day
+    $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
     // Timeblock past, present, future
-    // IF else statement
-    // 
+    
+    // using JQuery to loop through all elements with the class time block
     $(".time-block").each(function() {
       console.log($(this).attr("id"));
+      // get the id of the current timeblock which represents a time
+      // parseInt to convert string to integer
       var timeblock = parseInt($(this).attr("id"));
       console.log(currentTime);
 
+      // IF else statement to determine if timeblock is past, present, or future
       if (timeblock < currentTime) {
         $(this).addClass("past");
       } else if (timeblock === currentTime) {
@@ -22,37 +28,35 @@ var currentTime = dayjs().hour();
       }
     });
 
+    // delegation for saving textareas
+    // event listener for save button
+    $(document).on("click", ".saveBtn", function(event) {
+      // finds closest time block to the clicked button
+      var parentBlock = $(this).closest(".time-block");
+      var textarea = parentBlock.find("textarea");
+      // getting value of text area and id of time block
+      var text = textarea.val();
+      var id = parentBlock.attr("id");
+      // saving text area to local storage
+      localStorage.setItem("textarea" + id, text);
+    });
 
-     // Entering events
-    // Saving to local storage
-    // doesn't dissapear when refreshing the page
-    // make it save for each time block
+    // load textareas from local storage
+    $(".time-block").each(function() {
+      // gets saved text based on id and sets value to saved text
+      // saves when refreshing page
+      var id = $(this).attr("id");
+      var savedText = localStorage.getItem("textarea" + id);
+      var textarea = $(this).find("textarea");
+      textarea.val(savedText);
+    });
 
-      var textarea = document.getElementById("textarea");
-      textarea.value = localStorage.getItem("textarea");
-      });
-
-     
-    
-
-
-   
-
+    //   var textarea = document.getElementById("textarea");
+    //   textarea.value = localStorage.getItem("textarea");
+    //   });
 
 
-
-  
-
-
-
-
-   
-
-
-
-   
-
-    $("button").on("click", function(event) {
-      var textarea = document.getElementById("textarea");
-      localStorage.setItem("textarea", textarea.value);
+    // $("button").on("click", function(event) {
+    //   var textarea = document.getElementById("textarea");
+    //   localStorage.setItem("textarea", textarea.value);
     }) ;
